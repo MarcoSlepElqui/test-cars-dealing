8187c13ec04f8113e833447d448160d9dd614088
Marco Vega Rojas <marco.vega@slepelqui.gob.cl>
2026-06-23 18:06:03 -0400

    fin del dia

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,23 +1,29 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { get } from 'http';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-    private cars= ['Toyota', 'Honda', 'Jeep'];
+   
+    constructor( 
+        private readonly carsServices : CarsService
+    ){}
     @Get()
     getAllCars(){
-        return this.cars;
+        return this.carsServices.findAll();
     }
 
 
     @Get(':id')
-    getCarById(@Param('id') id2:string){
+    getCarById(@Param('id') id2:string){/*
         console.log({id2});
         console.log('id = '+id2);/// lo mismo
         if (+id2 < 2) {
             return this.cars[+id2];
         }else   
-            return "Número mayor que el arreglo";
+            return "Número mayor que el arreglo";*/
+        console.log({id:+id2});
+        return this.carsServices.findOneByID(+id2);
             
     }
 }
