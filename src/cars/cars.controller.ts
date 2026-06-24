e38d4ef860fcaff35631c24bfc8f302e432fad69
Marco Vega <marco.vega@slepelqui.gob.cl>
2026-06-24 00:44:46 -0400

    Update cars.controller.ts

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, MethodNotAllowedException, Param , ParseIntPipe, Post,Body, Patch, Delete} from '@nestjs/common';
 import { get } from 'http';
 import { CarsService } from './cars.service';
 
@@ -15,7 +15,7 @@ export class CarsController {
 
 
     @Get(':id')
-    getCarById(@Param('id') id2:string){/*
+    getCarById(@Param('id', ParseIntPipe) id2:number){/*
         console.log({id2});
         console.log('id = '+id2);/// lo mismo
         if (+id2 < 2) {
@@ -23,7 +23,36 @@ export class CarsController {
         }else   
             return "Número mayor que el arreglo";*/
         console.log({id:+id2});
-        return this.carsServices.findOneByID(+id2);
+        return this.carsServices.findOneByID(id2);
             
     }
+
+    @Post()
+    createCar(@Body() body:any){
+       /* return {
+            ok: "Creando un auto",
+            method: 'POST'
+        };*/
+        return body;
+    }  
+    
+    @Patch(':id')
+    updateCar(
+            @Param('id') id: number, 
+            @Body() body:any
+        ){
+      
+        return body;
+    }  
+    
+     @Delete(':id')
+    deleteCar(@Param('id') id: number){
+      
+        return {
+            ok: "Eliminando un auto",
+            method: 'DELETE',
+            id: id  
+        };
+    }  
+
 }
